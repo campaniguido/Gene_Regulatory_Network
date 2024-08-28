f596b6831c91c1adc35ee7a298ddd183d14b7843
Guido Campani <ieo6943@MBP-di-Campani.ieo.it>
2024-08-28 11:28:13 +0200

    debugging

--- GRN_create.py
+++ GRN_create.py
@@ -6,8 +6,21 @@ from GENIE3 import GENIE3
 import umap
 import matplotlib.pyplot as plt
 import sys
+import psutil
 #import dask.dataframe as dd
 
+###############################################
+#Managing resources
+###############################################
+def log_resources(stage):
+    memory_used = psutil.Process().memory_info().rss / (1024 ** 3)
+    cpu_percent = psutil.cpu_percent(interval=1)
+    with open("resource_usage.log", "a") as log_file:
+        log_file.write(f"{stage}: Memoria usata: {memory_used:.2f} GB, "
+                       f"CPU: {cpu_percent}%\n")
+#################################################
+
+log_resources("begining Script")
 
 #path_h5ad = '/Users/ieo6943/Documents/Guido/Albi/clustered.h5ad/MDA_chemo/data/SCT/clustered.h5ad'
 #path_results = '/Users/ieo6943/Downloads/'
@@ -19,22 +32,26 @@ path_genes = '/hpcnfs/scratch/PGP/acossa/breast_albi/MDA/single_cell/scripts/GRN
 
 origin = sys.argv[1]
 treatment = sys.argv[2]
-nthreads = sys.argv[3]
+nthreads = int(sys.argv[3])
 type_GRN = sys.argv[4]
 
-origin = 'PT'
-treatment = 'treated'
-nthreads = int(15)
+#origin = 'PT'
+#treatment = 'treated'
+nthreads = int(20)
 
 os.makedirs(path_results, exist_ok=True)
 
+log_resources("before ANNdata")
 cluster_h5ad = sc.read_h5ad(path_h5ad)
+log_resources("after ANNdata")
+
 genes = list(pd.read_csv(path_genes, index_col=0).index)
 
 adata_NT = cluster_h5ad[cluster_h5ad.obs['condition']==f'{origin}, {treatment}']
 adata_NT = cluster_h5ad[:, cluster_h5ad.var_names.isin(genes)]
 expression_data = pd.DataFrame(adata_NT.X.toarray(), index=adata_NT.obs_names, columns=adata_NT.var_names)
 
+cluster_h5ad.obs['condition'].unique()
 #Corr matrix
 #if type_GRN=='corr':
 #    #corr_expression_data = expression_data.corr()
@@ -47,9 +64,11 @@ expression_data = pd.DataFrame(adata_NT.X.toarray(), index=adata_NT.obs_names, c
 print(nthreads,'<-cores')
 print ('type nthreads:', nthreads)
 
+log_resources("before Network")
 if type_GRN=='GRN':
     grn_matrix = GENIE3(expression_data.values, nthreads=nthreads)
     np.savetxt(os.path.join(path_results, f'GRN_{origin}_{treatment}.csv'), grn_matrix, delimiter=',')
+log_resources("After Network")
 
 
 
