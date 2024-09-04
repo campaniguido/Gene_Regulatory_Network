47e37c8fd5b2c9c6168ff69d650b89f93344296a
Guido Campani <ieo6943@MacBook-Pro-di-Campani.local>
2024-09-04 10:23:04 +0200

    all data

--- GRN_create.py
+++ GRN_create.py
@@ -9,19 +9,6 @@ import sys
 import psutil
 #import dask.dataframe as dd
 
-###############################################
-#Managing resources
-###############################################
-#def log_resources(stage):
-#    memory_used = psutil.Process().memory_info().rss / (1024 ** 3)
-#    cpu_percent = psutil.cpu_percent(interval=1)
-#    with open("resource_usage.log", "a") as log_file:
-#        log_file.write(f"{stage}: Memoria usata: {memory_used:.2f} GB, "
-#                       f"CPU: {cpu_percent}%\n")
-#################################################
-
-#log_resources("begining Script")
-
 #path_h5ad = '/Users/ieo6943/Documents/Guido/Albi/clustered.h5ad/MDA_chemo/data/SCT/clustered.h5ad'
 ##
 #path_genes = '/Users/ieo6943/Downloads/genes.csv'
@@ -41,20 +28,17 @@ n_trees = int(sys.argv[6])
 #treatment = 'treated'
 nthreads = int(20)
 
-
-#path_results = f'/Users/ieo6943/Documents/Guido/Albi/data_my_GRN/{size_subset}'
 os.makedirs(path_results, exist_ok=True)
 
-#log_resources("before ANNdata")
 cluster_h5ad = sc.read_h5ad(path_h5ad)
-#log_resources("after ANNdata")
 
 genes = list(pd.read_csv(path_genes, index_col=0).index)
 
 adata_NT = cluster_h5ad[cluster_h5ad.obs['condition']==f'{origin}, {treatment}']
 adata_NT = adata_NT[:, adata_NT.var_names.isin(genes)]
 expression_data = pd.DataFrame(adata_NT.X.toarray(), index=adata_NT.obs_names, columns=adata_NT.var_names)
-expression_data_sample = expression_data.sample(n=size_subset, random_state=2)
+#expression_data_sample = expression_data.sample(n=size_subset, random_state=2)
+expression_data_sample = expression_data
 
 
 #Corr matrix
@@ -74,36 +58,10 @@ zero_genes = expression_data_sample.iloc[:,zero_columns].columns
 filtered_data = expression_data_sample.iloc[:,non_zero_columns]
 filtered_data.to_csv(os.path.join(path_results, f'cell_{origin}_{treatment}.csv'), sep=',', index=True, header=True)
 
-#log_resources("before Network")
 if type_GRN=='GRN':
     grn_matrix = GENIE3(filtered_data.values, nthreads=nthreads,ntrees=n_trees)
     
     df_grn = pd.DataFrame(grn_matrix,index=filtered_data.columns, columns=filtered_data.columns)
     #is_symmetric = df_grn.equals(df_grn.T)
-    df_grn.to_csv(os.path.join(path_results, f'GRN_{origin}_{treatment}_{size_subset}_sample.csv'), sep=',', index=True, header=True)
+    df_grn.to_csv(os.path.join(path_results, f'GRN_{origin}_{treatment}_{size_subset}.csv'), sep=',', index=True, header=True)
 
-#log_resources("After Network")
-
-
-
-#UMAP
-#conditions = list(adata_NT.obs['condition'].unique())
-#colors = ['red', 'blue', 'green', 'orange', 'purple']
-#condition_color_dict = dict(zip(conditions, colors))
-#
-#colors = adata_NT.obs['condition'].map(condition_color_dict)
-#
-#
-#
-## Esegui UMAP
-#reducer = umap.UMAP(n_components=2)
-#embedding = reducer.fit_transform(expression_data)
-#
-## Visualizza i risultati
-#plt.figure(figsize=(10, 8))
-#plt.scatter(embedding[:, 0], embedding[:, 1], c=colors, s=0.1)
-#plt.title("UMAP Projection")
-#plt.xlabel("UMAP1")
-#plt.ylabel("UMAP2")
-#plt.show()
-#
